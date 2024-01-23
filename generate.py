44116da0471dbd9247f8d5e4d72ae2e882931adb
asaurusrex <58984065+asaurusrex@users.noreply.github.com>
2024-01-23 15:52:48 -0800

    Update generate.py
    bug fixes related to path names being off, improper function ordering (off by 1)

--- generate.py
+++ generate.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 import sys
-#requires use of VS Code Debug or shell with dumpbin in path, also requires you to specify path to DLL as argument
-dll_to_proxy = sys.argv[1]
+
+dll_to_proxy = sys.argv[1] 
 
 process_out = subprocess.run("dumpbin /exports {} > dll_export.txt".format(dll_to_proxy),shell=True)
 #print(type(process_out.stdout))
@@ -10,6 +10,9 @@ process_out = subprocess.run("dumpbin /exports {} > dll_export.txt".format(dll_t
 
 #list_info = process_out.stdout.decode().split("\r\n")
 #print(type(process_out.stdout))
+
+#remove the .dll from filename
+dll_to_proxy = dll_to_proxy[:-4]
 with open('dll_export.txt', 'r+') as f:
     number_functions = 0
     lines = f.readlines()
@@ -22,13 +25,19 @@ with open('dll_export.txt', 'r+') as f:
             #print(number_functions)
         if "ordinal hint RVA" in lines[i]:
             for y in range(int(number_functions)*2 + 1):
+                w = y - 1
                 #print(y)
                 if lines[i+y] != " " and y!= 0 and lines[i+y].strip() != "Summary":
                     if len(lines[i+y].strip().split(" ")) != 1:
-                        #print(lines[i+y].strip().split(" ")[-1])
+                        #print(lines[i+y].strip().split(" "))
                         functionname = lines[i+y].strip().split(" ")[-1]
                         ##pragma comment(linker,"/export:SystemFunction002=C:\\Windows\\System32\\cryptbaseGetFileVersionInfoA,@1")
-                        string_of_interest = "#pragma comment(linker,\"/export:{0}=C:\\\\Windows\\\\System32\\\\{1}.{2},@{3}\")".format(functionname, dll_to_proxy[:-4], functionname, y)
+                        if "\\" in dll_to_proxy:
+                            list_dll_to_proxy = dll_to_proxy.split("\\")
+                            #print(list_dll_to_proxy)
+                            dll_to_proxy = list_dll_to_proxy[-1] #should be the final element
+                        
+                        string_of_interest = "#pragma comment(linker,\"/export:{0}=C:\\\\Windows\\\\System32\\\\{1}.{2},@{3}\")".format(functionname, dll_to_proxy, functionname, w)
                         print(string_of_interest)
                 if lines[i+y].strip() == "Summary":
                     break
